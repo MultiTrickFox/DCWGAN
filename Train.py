5cbd83d531b1f70e56d42d13f60dbfbe8fcc95ee
K <31030673+FirefoxIstSchon@users.noreply.github.com>
2018-12-19 20:50:08 +0300

    epoch cycle done.

--- Train.py
+++ Train.py
@@ -37,46 +37,48 @@ discriminator = Models.Discriminator(width, height, hm_filters2, hm_filters1)
 
 for i in range(hm_epochs):
 
-    # fake_data = generator.forward(batchsize=batches_of)       # todo : unlock when data comes in .
-    # real_data = res.get_data(hm_data)
-    #
-    # fake_set = tuple([(data, 0) for data in fake_data])
-    # real_set = tuple([(data, 1) for data in real_data])
-    #
-    # dataset = tuple(random.shuffle([e for e in fake_set + real_set]))
-    #
-    #
-    # epoch_loss_disc = 0
-    # for sample, label in dataset: # todo no sample label!!!
-    #
-    #     discriminator_result = discriminator.forward(sample)
-    #     loss = Models.loss_discriminator(discriminator_result, label)
-    #
-    #     Models.update(loss, discriminator, generator, update_for='discriminator', lr=learning_rate, batch_size=batches_of)
-    #
-    #     epoch_loss_disc += float(loss)
-    # print(f'Epoch {i} Loss Disc: {epoch_loss_disc}')
+    epoch_loss_gen, epoch_loss_disc = 0, 0
+
+    for j in range(int(hm_data/batches_of)):
+
+        # fake_data = generator.forward(batchsize=batches_of)       # todo : unlock when data comes in .
+        # real_data = res.get_data(hm_data)
+        #
+        # fake_set = tuple([(data, 0) for data in fake_data])
+        # real_set = tuple([(data, 1) for data in real_data])
+        #
+        # dataset = tuple(random.shuffle([e for e in fake_set + real_set]))
+        #
+        #
+
+        # for sample, label in dataset: # todo no sample label!!!
+        #
+        #     discriminator_result = discriminator.forward(sample)
+        #     loss = Models.loss_discriminator(discriminator_result, label)
+        #
+        #     Models.update(loss, discriminator, generator, update_for='discriminator', lr=learning_rate, batch_size=batches_of)
+        #
+        #     epoch_loss_disc += float(loss)
+        # print(f'Epoch {i} Loss Disc: {epoch_loss_disc}')
 
 
 
-    constructed_data = generator.forward(batchsize=batches_of)
-    discriminator_result = discriminator.forward(constructed_data)
+        constructed_data = generator.forward(batchsize=batches_of)
+        discriminator_result = discriminator.forward(constructed_data)
 
-    epoch_loss_gen = 0
+        if gen_maximize_loss:
+            loss = Models.loss_generator(discriminator_result, loss_type='maximize')
+            epoch_loss_gen += float(loss)
 
-    if gen_maximize_loss:
-        loss = Models.loss_generator(discriminator_result, loss_type='maximize')
-        epoch_loss_gen += float(loss)
+            Models.update(loss, discriminator, generator, update_for='generator', maximize_loss=True, lr=learning_rate, batch_size=batches_of)
 
-        Models.update(loss, discriminator, generator, update_for='generator', maximize_loss=True, lr=learning_rate, batch_size=batches_of)
+        else:
+            loss = Models.loss_generator(discriminator_result)
+            epoch_loss_gen += float(loss)
 
-    else:
-        loss = Models.loss_generator(discriminator_result)
-        epoch_loss_gen += float(loss)
+            Models.update(loss, discriminator, generator, update_for='generator', lr=learning_rate, batch_size=batches_of)
 
-        Models.update(loss, discriminator, generator, update_for='generator', lr=learning_rate, batch_size=batches_of)
-
-    print(f'Epoch {i} Loss Gen: {epoch_loss_gen}')
+        print(f'Epoch {i} Loss Gen: {epoch_loss_gen}')
 
 
 res.pickle_save(discriminator, 'discriminator.pkl')
